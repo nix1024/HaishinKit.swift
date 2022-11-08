af54677a7d731e7db515fd33fca547404456549c
Nix <wangxin.pro@bytedance.com>
2022-11-08 15:06:33 +0800

    Refactor
    Change-Id: Id43eaf2d88083537e30ef9060359a99a938b958a

---
+++ Sources/HTTPFLV/HTTPFLVMuxer.swift
@@ -0,0 +1,89 @@
+//
+//  HTTPFLVMuxer.swift
+//  HaishinKit
+//
+//  Created by Nix Wang on 2022/11/8.
+//
+
+import AVFoundation
+
+protocol HTTPFLVMuxerDelegate: AnyObject {
+
+    func muxer(_ muxer: HTTPFLVMuxer, didSetMetadata: ASObject)
+
+    func muxer(_ muxer: HTTPFLVMuxer, didOutputAudio buffer: Data, withTimestamp: Double)
+
+    func muxer(_ muxer: HTTPFLVMuxer, didOutputVideo buffer: Data, withTimestamp: Double)
+
+    func muxer(_ muxer: HTTPFLVMuxer, videoCodecErrorOccurred error: VideoCodec.Error)
+
+}
+
+class HTTPFLVMuxer {
+
+    weak var delegate: HTTPFLVMuxerDelegate?
+
+    private var videoTimeStamp = CMTime.zero
+
+    init() {
+
+    }
+
+    func dispose() {
+        videoTimeStamp = CMTime.zero
+    }
+
+}
+
+extension HTTPFLVMuxer: AVCodecDelegate {
+
+    func audioCodec(_ codec: AudioCodec, didSet formatDescription: CMFormatDescription?) {
+
+    }
+
+    func audioCodec(_ codec: AudioCodec, didOutput sample: UnsafeMutableAudioBufferListPointer, presentationTimeStamp: CMTime) {
+
+    }
+
+    func videoCodec(_ codec: VideoCodec, didSet formatDescription: CMFormatDescription?) {
+        guard
+            let formatDescription = formatDescription,
+            let avcC = AVCConfigurationRecord.getData(formatDescription) else {
+            return
+        }
+        var buffer = Data([FLVFrameType.key.rawValue << 4 | FLVVideoCodec.avc.rawValue, FLVAVCPacketType.seq.rawValue, 0, 0, 0])
+        buffer.append(avcC)
+        delegate?.muxer(self, didOutputVideo: buffer, withTimestamp: 0)
+    }
+
+    func videoCodec(_ codec: VideoCodec, didOutput sampleBuffer: CMSampleBuffer) {
+        let keyframe: Bool = !sampleBuffer.isNotSync
+        var compositionTime: Int32 = 0  // compositionTime = pts - dts
+        let presentationTimeStamp: CMTime = sampleBuffer.presentationTimeStamp  // ts for display
+        var decodeTimeStamp: CMTime = sampleBuffer.decodeTimeStamp  // ts for decoding, may smaller than pts for P/B frames
+
+        if decodeTimeStamp == CMTime.invalid {
+            decodeTimeStamp = presentationTimeStamp
+        } else {
+            compositionTime = (videoTimeStamp == .zero) ? 0 : Int32((sampleBuffer.presentationTimeStamp.seconds - videoTimeStamp.seconds) * 1000)
+        }
+        let delta = (videoTimeStamp == CMTime.zero ? 0 : decodeTimeStamp.seconds - videoTimeStamp.seconds) * 1000
+        guard let data = sampleBuffer.dataBuffer?.data, 0 <= delta else {
+            return
+        }
+        var buffer = Data([
+            ((keyframe ? FLVFrameType.key.rawValue : FLVFrameType.inter.rawValue) << 4) | FLVVideoCodec.avc.rawValue,
+            FLVAVCPacketType.nal.rawValue
+        ])
+        buffer.append(contentsOf: compositionTime.bigEndian.data[1..<4])
+        buffer.append(data)
+        delegate?.muxer(self, didOutputVideo: buffer, withTimestamp: presentationTimeStamp.seconds * 1000)
+        videoTimeStamp = decodeTimeStamp
+
+    }
+
+    func videoCodec(_ codec: VideoCodec, errorOccurred error: VideoCodec.Error) {
+        delegate?.muxer(self, videoCodecErrorOccurred: error)
+    }
+
+}
--- Sources/HTTPFLV/HTTPFLVStream.swift
+++ Sources/HTTPFLV/HTTPFLVStream.swift
@@ -34,7 +34,7 @@ public class HTTPFLVStream {
     private var serializer: AMFSerializer = AMF0Serializer()
 
     private var previousTagSize: UInt32 = 0
-    private var firstDecodeTimeStamp: Double?
+    private var firstDecodeTimeStamp: Double?   // TODO: is this necessary?
 
     public init() {
         muxer.delegate = self
@@ -58,7 +58,7 @@ public class HTTPFLVStream {
             0x56,   // V
             0x01,   // version 1
             0x01,   // video only
-            0x00, 0x00, 0x00, 0x09,     // header size
+            0x00, 0x00, 0x00, 0x09, // header size
         ])
     }
     
@@ -158,86 +158,4 @@ extension HTTPFLVStream: Running {
             self.videoIO.stopEncoding()
         }
     }
-}
-
-
-protocol HTTPFLVMuxerDelegate: AnyObject {
-    
-    func muxer(_ muxer: HTTPFLVMuxer, didSetMetadata: ASObject)
-    
-    func muxer(_ muxer: HTTPFLVMuxer, didOutputAudio buffer: Data, withTimestamp: Double)
-    
-    func muxer(_ muxer: HTTPFLVMuxer, didOutputVideo buffer: Data, withTimestamp: Double)
-    
-    func muxer(_ muxer: HTTPFLVMuxer, videoCodecErrorOccurred error: VideoCodec.Error)
-
-}
-
-class HTTPFLVMuxer {
-    
-    weak var delegate: HTTPFLVMuxerDelegate?
-    
-    private var videoTimeStamp = CMTime.zero
-    
-    init() {
-        
-    }
-    
-    func dispose() {
-        videoTimeStamp = CMTime.zero
-    }
-    
-}
-
-extension HTTPFLVMuxer: AVCodecDelegate {
-    
-    func audioCodec(_ codec: AudioCodec, didSet formatDescription: CMFormatDescription?) {
-        
-    }
-    
-    func audioCodec(_ codec: AudioCodec, didOutput sample: UnsafeMutableAudioBufferListPointer, presentationTimeStamp: CMTime) {
-        
-    }
-    
-    func videoCodec(_ codec: VideoCodec, didSet formatDescription: CMFormatDescription?) {
-        guard
-            let formatDescription = formatDescription,
-            let avcC = AVCConfigurationRecord.getData(formatDescription) else {
-            return
-        }
-        var buffer = Data([FLVFrameType.key.rawValue << 4 | FLVVideoCodec.avc.rawValue, FLVAVCPacketType.seq.rawValue, 0, 0, 0])
-        buffer.append(avcC)
-        delegate?.muxer(self, didOutputVideo: buffer, withTimestamp: 0)
-    }
-    
-    func videoCodec(_ codec: VideoCodec, didOutput sampleBuffer: CMSampleBuffer) {
-        let keyframe: Bool = !sampleBuffer.isNotSync
-        var compositionTime: Int32 = 0  // compositionTime = pts - dts
-        let presentationTimeStamp: CMTime = sampleBuffer.presentationTimeStamp  // ts for display
-        var decodeTimeStamp: CMTime = sampleBuffer.decodeTimeStamp  // ts for decoding, may smaller than pts for P/B frames
-        
-        if decodeTimeStamp == CMTime.invalid {
-            decodeTimeStamp = presentationTimeStamp
-        } else {
-            compositionTime = (videoTimeStamp == .zero) ? 0 : Int32((sampleBuffer.presentationTimeStamp.seconds - videoTimeStamp.seconds) * 1000)
-        }
-        let delta = (videoTimeStamp == CMTime.zero ? 0 : decodeTimeStamp.seconds - videoTimeStamp.seconds) * 1000
-        guard let data = sampleBuffer.dataBuffer?.data, 0 <= delta else {
-            return
-        }
-        var buffer = Data([
-            ((keyframe ? FLVFrameType.key.rawValue : FLVFrameType.inter.rawValue) << 4) | FLVVideoCodec.avc.rawValue,
-            FLVAVCPacketType.nal.rawValue
-        ])
-        buffer.append(contentsOf: compositionTime.bigEndian.data[1..<4])
-        buffer.append(data)
-        delegate?.muxer(self, didOutputVideo: buffer, withTimestamp: presentationTimeStamp.seconds * 1000)
-        videoTimeStamp = decodeTimeStamp
-        
-    }
-    
-    func videoCodec(_ codec: VideoCodec, errorOccurred error: VideoCodec.Error) {
-        delegate?.muxer(self, videoCodecErrorOccurred: error)
-    }
-    
 }
